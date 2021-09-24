464d81642462cee1900250280c4f47d3eff5e03b
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-24 19:02:38 +0330

    add react query provider and default configs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./assets/scss/main.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { DEFAULT_QUERY_OPTIONS } from "./constants/base";
+const queryClient = new QueryClient(DEFAULT_QUERY_OPTIONS);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
