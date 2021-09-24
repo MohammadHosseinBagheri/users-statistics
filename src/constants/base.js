9506f7043c9dadd32dc2f1934a24e118f8e95493
mohammadhosseinbagheri <mohammadbagheri228@gmail.com>
2021-09-24 19:06:45 +0330

    add base url and default query options

---
+++ src/constants/base.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "https://randomuser.me";
+export const DEFAULT_QUERY_OPTIONS = {
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+};
